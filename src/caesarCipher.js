56b6d098f64c7261566fb32f2a5c53e5eead8a20
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-12 21:09:19 -0700

    Break code into smaller functions

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,10 +1,6 @@
 export function caesarCipher(text, shiftValue) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  const adjustedShift = shiftValue % alphabet.length;
-
-  const alphabetFront = alphabet.slice(adjustedShift);
-  const alphabetBack = alphabet.slice(0, adjustedShift);
-  const caesarAlphabet = alphabetFront.concat(alphabetBack);
+  const caesarAlphabet = createCaesarAlphabet(alphabet, shiftValue);
 
   const cipher = [];
   text.split("").forEach((char) => {
@@ -16,8 +12,16 @@ export function caesarCipher(text, shiftValue) {
     if (isUpperCase(char)) cipherChar = cipherChar.toUpperCase();
     cipher.push(cipherChar);
   });
-
   return cipher.join("");
+}
+
+function createCaesarAlphabet(letters, shiftValue) {
+  const alphabet = typeof letters == "string" ? letters.split("") : letters;
+  const adjustedShiftValue = shiftValue % alphabet.length;
+
+  const alphabetFront = alphabet.slice(adjustedShiftValue);
+  const alphabetBack = alphabet.slice(0, adjustedShiftValue);
+  return alphabetFront.concat(alphabetBack);
 }
 
 function isUpperCase(char) {
