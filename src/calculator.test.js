8adf3454e9af80569fe136e5f394c7e032f67230
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-12 17:09:40 -0700

    Complete multiplciation with tests

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -5,7 +5,6 @@ describe("Calculator Tests", () => {
     it("Will add 2 + 2", () => {
       expect(calc.add(2, 2)).toBe(4);
     });
-
     it("Will add 3 + -10", () => {
       expect(calc.add(3, -10)).toBe(-7);
     });
@@ -15,9 +14,26 @@ describe("Calculator Tests", () => {
     it("Will minus 1 - 1", () => {
       expect(calc.subtract(1, 1)).toBe(0);
     });
-
     it("Will minus 10 - -1", () => {
       expect(calc.subtract(10, -1)).toBe(11);
+    });
+  });
+
+  describe("Divide tests", () => {
+    it("Will divide 10 / 2", () => {
+      expect(calc.divide(10, 2)).toBe(5);
+    });
+    it("Handle decimals", () => {
+      expect(calc.divide(10.5, 2)).toBeCloseTo(5.25);
+    });
+    it("Will deal with one negative number", () => {
+      expect(calc.divide(10, -20)).toBeCloseTo(-0.5);
+    });
+    it("Will deal with two negative numbers", () => {
+      expect(calc.divide(-20, -20)).toBeCloseTo(1);
+    });
+    it("Can handle zero", () => {
+      expect(calc.divide(10, 0)).toBe(Infinity);
     });
   });
 });
